58a1e387e2fc341b17a7294b70291d924e4c3d92
Hedwin <hedwin.coursimault@gmail.com>
2021-11-24 13:45:44 +0100

    bouton upload et header

--- app-vue/src/store/auth/auth-module.store.ts
+++ app-vue/src/store/auth/auth-module.store.ts
@@ -1,7 +1,7 @@
 import AuthMutationTypes from "@/store/auth/auth-mutation-types";
 import axios from "axios";
 import AuthActionTypes from "@/store/auth/auth-action-types";
-import {Constants} from "@/constants/Constants";
+import { Constants } from "@/constants/Constants";
 
 interface State {
     isConnected: boolean;
@@ -15,8 +15,8 @@ const authModule = {
         key: ""
     }),
     actions: {
-        async [AuthActionTypes.TRY_CONNECTION]({commit}: any, auth: any){
-            await axios.post(`${Constants.WEB_URL}/${Constants.CONNECTION}`,auth)
+        async [AuthActionTypes.TRY_CONNECTION]({ commit }: any, auth: any) {
+            await axios.post(`${Constants.WEB_URL}/${Constants.CONNECTION}`, auth)
                 .then((response: any) => {
                     commit(AuthMutationTypes.UPDATE_CONNECITON, true);
                     commit(AuthMutationTypes.UPDATE_KEY, "");
@@ -27,10 +27,10 @@ const authModule = {
         }
     },
     mutations: {
-        [AuthMutationTypes.UPDATE_CONNECITON](state: State, isConnected: boolean){
+        [AuthMutationTypes.UPDATE_CONNECITON](state: State, isConnected: boolean) {
             state.isConnected = isConnected;
         },
-        [AuthMutationTypes.UPDATE_KEY](state: State, key: string){
+        [AuthMutationTypes.UPDATE_KEY](state: State, key: string) {
             state.key = key;
         }
     }
