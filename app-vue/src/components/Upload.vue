b58e6412d5d98fcf20d0d8450c2a187179cfffe9
Hedwin <hedwin.coursimault@gmail.com>
2021-11-24 10:40:28 +0100

    upload vue

--- app-vue/src/components/Upload.vue
+++ app-vue/src/components/Upload.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="w-100 h-100 text-center">
-    <button type="button" class="btn btn-primary">
-      <span class="glyphicon glyphicon-upload"></span> Upload
-    </button>
+  <div class="w-100 h-100 text-center d-flex flex-column">
+    <div class="form-group m-2">
+      <input type="file" id="formNomFichier" />
+    </div>
+    <div class="w-100">
+      <button type="submit" class="btn w-25 btn-primary">
+        <span class="glyphicon glyphicon-upload"></span> Upload
+      </button>
+    </div>
   </div>
 </template>
 
