d5f649bb6a40c8f1b5f21af678177324b6cf8795
Valentin Huard <valentin.huard@autobonplan.com>
2021-11-25 13:55:50 +0100

    fix debug

--- app-vue/src/components/Login.vue
+++ app-vue/src/components/Login.vue
@@ -3,11 +3,8 @@
         <div class="row py-5 mt-4 align-items-center">
             <!-- For Demo Purpose -->
             <div class="col-md-5 pr-lg-5 mb-5 mb-md-0">
-              <img src="../assets/rhino.png"
-                  alt=""
-                  class="img-fluid mb-3 d-none d-md-block"
-              />
-            </div>h1>Accédez à votre RhinoDrive</h1>
+              <img src="../assets/rhino.png" alt="" class="img-fluid mb-3 d-none d-md-block" />
+                <h1>Accédez à votre RhinoDrive</h1>
             </div>
 
             <!-- Registeration Form -->
@@ -24,23 +21,21 @@
                             <input id="email" type="email" name="email" placeholder="Adresse mail" class="form-control bg-white border-left-0 border-md" v-model="login">
                         </div>
 
-            <!-- Divider Text -->
-            <div
-              class="
-                form-group
-                col-lg-12
-                mx-auto
-                d-flex
-                align-items-center
-                my-4
-              "
-            >
-              <div class="border-bottom w-100 ml-5"></div>
-              <span class="px-2 small text-muted font-weight-bold text-muted"
-                >Ou</span
-              >
-              <div class="border-bottom w-100 mr-5"></div>
-            </div>
+                        <!-- Password -->
+                        <div class="input-group col-lg-6 mb-4">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text bg-white px-4 border-md border-right-0">
+                                    <i class="fa fa-lock text-muted"></i>
+                                </span>
+                            </div>
+                            <input id="password" type="password" name="password" placeholder="Votre mot de passe" class="form-control bg-white border-left-0 border-md" v-model="passwd">
+                        </div>
+                        <!-- Submit Button -->
+                        <div class="form-group col-lg-12 mx-auto mb-0">
+                            <a @click="connection()" href="#" class="btn btn-primary btn-block py-2">
+                                <span class="font-weight-bold">Créer votre compte</span>
+                            </a>
+                        </div>
 
                         <!-- Divider Text -->
                         <div class="form-group col-lg-12 mx-auto d-flex align-items-center my-4">
@@ -135,36 +130,33 @@
             </div>
         </div>
     </div>
-  </div>
 </template>
 
 <script>
-import { defineComponent } from "vue";
-import { mapActions } from "vuex";
-import { Constants } from "@/constants/Constants";
+import {defineComponent} from "vue";
+import {mapActions} from "vuex";
+import {Constants} from "@/constants/Constants";
 import AuthActionTypes from "@/store/auth/auth-action-types";
 
 export default defineComponent({
   name: "Signin",
-  data() {
+  data(){
     return {
-      currentComponent: "connexion",
+      currentComponent: 'connexion',
       login: "",
-      passwd: "",
-    };
+      passwd: ""
+    }
   },
   methods: {
     ...mapActions(Constants.AUTH_STORE, [AuthActionTypes.TRY_CONNECTION]),
-    connection() {
-      this[AuthActionTypes.TRY_CONNECTION]({
-        login: this.login,
-        password: this.passwd,
-      });
+    connection(){
+      this[AuthActionTypes.TRY_CONNECTION]({login: this.login, password: this.passwd});
     },
-    swapComponent: function (component) {
-      console.log(component);
+    swapComponent: function(component)
+    {
+      console.log(component)
       this.currentComponent = component;
-    },
-  },
-});
+    }
+  }
+})
 </script>
